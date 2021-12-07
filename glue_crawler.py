32be8f0a5b0b12f0aa1a2b924cee3a054f805e80
ASammyA <ahmad.sammy.a@gmail.com>
2021-12-07 14:13:18 +0000

    commented out glue_crawler dag to work on bash dag

--- glue_crawler.py
+++ glue_crawler.py
@@ -1,24 +1,27 @@
-from airflow.models.BaseOperator import AwsGlueCrawlerOperator
+# import boto3
 
-DEFAULT_ARGS = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': airflow.utils.dates.days_ago(0),
-    'email': ['ahmad.sammy.a@gmail.com'],
-    'email_on_failure': True,
-    'email_on_retry': False
-}
+# DEFAULT_ARGS = {
+#     'owner': 'airflow',
+#     'depends_on_past': False,
+#     'start_date': airflow.utils.dates.days_ago(0),
+#     'email': ['ahmad.sammy.a@gmail.com'],
+#     'email_on_failure': True,
+#     'email_on_retry': False
+# }
 
-dag = DAG(
-    'emr_job_flow_manual_steps_dag_pyspark',
-    default_args=DEFAULT_ARGS,
-    dagrun_timeout=timedelta(hours=2),
-    schedule_interval=None
-)
+# dag = DAG(
+#     'emr_job_flow_manual_steps_dag_pyspark',
+#     default_args=DEFAULT_ARGS,
+#     dagrun_timeout=timedelta(hours=2),
+#     schedule_interval=None
+# )
 
-glue_crawler = AwsGlueCrawlerOperator(
-    task_id='glue_crawler',
-    dag=dag
-)
+# session=boto3.client('emr','us-east-1')
+# def create_glue_crawler(): 
 
-glue_crawler
+# glue_crawler = AwsGlueCrawlerOperator(
+#     task_id='glue_crawler',
+#     dag=dag
+# )
+
+# glue_crawler
