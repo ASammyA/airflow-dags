cd6f3c149d5fcd71f20658e0f56f3a3925bbc0ea
ASammyA <ahmad.sammy.a@gmail.com>
2021-12-07 14:07:11 +0000

    created test dag for emr creation using bash

---
+++ create_emr_bash.py
@@ -0,0 +1,25 @@
+from airflow.models.BaseOperator import BashOperator
+
+DEFAULT_ARGS = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': airflow.utils.dates.days_ago(0),
+    'email': ['ahmad.sammy.a@gmail.com'],
+    'email_on_failure': True,
+    'email_on_retry': False
+}
+
+dag = DAG(
+    'emr_job_flow_manual_steps_dag_pyspark',
+    default_args=DEFAULT_ARGS,
+    dagrun_timeout=timedelta(hours=2),
+    schedule_interval=None
+)
+
+create_emr_bash = BashOperator(
+    task_id='create_emr_bash',
+    dag=dag,
+    bash_command="/opt/airflow/aws_cli_create_emr.sh"
+)
+
+create_emr_bash
